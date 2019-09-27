a883a9c8a7bb7810b3de63c12ac0956be3450ccf
lroudge <lauradrc@hotmail.fr>
2019-09-27 20:34:42 +0000

    Add script for task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function () {
-  
+  let amenityIds = {};
+  $("input[type=checkbox]").click(function () {
+    if ($(this).prop("checked")) {
+      amenityIds[$(this).attr("data-id")] = $(this).attr("data-name");
+    } else if (!$(this).prop("checked")) {
+      delete amenityIds[$(this).attr("data-id")];
+    }
+    $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+  });
 });
