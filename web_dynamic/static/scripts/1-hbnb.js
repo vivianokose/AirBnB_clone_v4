0a6a54725dd7a37714f62b60ec9cebc4421aa854
lroudge <lauradrc@hotmail.fr>
2019-09-28 19:26:07 +0000

    Add task 4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', function () {
-  let amenityIds = {};
-  $("input[type=checkbox]").click(function () {
-    if ($(this).prop("checked")) {
-      amenityIds[$(this).attr("data-id")] = $(this).attr("data-name");
-    } else if (!$(this).prop("checked")) {
-      delete amenityIds[$(this).attr("data-id")];
+  // task 2
+  const amenityIds = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
     }
     $('div.amenities h4').text(Object.values(amenityIds).join(', '));
   });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,20 @@
 window.addEventListener('load', function () {
+  // task 3
   $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
-    if (data.status === "OK") {
-      $("#api_status").addClass('available');
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
     } else {
-      $("#api_status").removeClass('available');
+      $('#api_status').removeClass('available');
     }
   });
-  let amenityIds = {};
-  $("input[type=checkbox]").click(function () {
-    if ($(this).prop("checked")) {
-      amenityIds[$(this).attr("data-id")] = $(this).attr("data-name");
-    } else if (!$(this).prop("checked")) {
-      delete amenityIds[$(this).attr("data-id")];
+
+  // task 2
+  const amenityIds = {};
+  $('input[type=checkbox]').click(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
     }
     $('div.amenities h4').text(Object.values(amenityIds).join(', '));
   });
